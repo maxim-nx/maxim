ad4b00a3e6dac40a3916cb277e554f6500b0f340
MaxMosier <maximillian.wx@gmail.com>
2023-05-12 00:31:54 -0600

    Renaming and Reformatting
    Some of these changes are just results of reformatting, but I've also changed some variable names to make things more confusing. Also rewrote one of the for loops to use the for-let-of instead of an iterator.

--- P5Projects/1-ART/pieces-ii.js
+++ P5Projects/1-ART/pieces-ii.js
@@ -5,17 +5,17 @@ class Piece {
 		this.framesToUpd8 = 1;
 		this.atDest = false;
 		this.destRange = 1;
-		this.availableAngles = [0, 90, 180, 270];
+		this.availableAngles = [-45, 45, -135, 135];
 		this.pathDecay = d;
 		this.xyAngles = [[], [], [], []]; // +x, -x, +y, -y
-		this.pPos = createVector(x,y);
+		this.pPos = createVector(x, y);
 		this.color = map(s, sRange[0], sRange[1], 0, 255);
 		this.transparency = map(s, sRange[0], sRange[1], 120, 255);
 	}
 
 	prepAngles() {
-		for (let i = 0; i < this.availableAngles.length; i++) {
-			let angle = this.availableAngles[i]+45;
+		for (let a of this.availableAngles) {
+			let angle = a;
 			const toRad = radians(angle);
 			if (cos(toRad) > 0) {
 				this.xyAngles[0].push(toRad);
@@ -31,7 +31,7 @@ class Piece {
 		}
 	}
 
-    modulatePos(xMax, yMax) {
+	modulatePos(xMax, yMax) {
 		this.pos.x = (this.pos.x + xMax) % xMax; //Had to add maxes to prevent negative modulo behaviour!
 		this.pos.y = (this.pos.y + yMax) % yMax;
 	}
@@ -51,13 +51,14 @@ class Piece {
 	update(dX, dY) {
 		const destVector = createVector(dX, dY);
 		if (this.pos.dist(destVector) <= this.destRange) {
-            this.atDest = true;
-          } else {
-            if (this.atDest) { // Piece just got freed
-                this.framesToUpd8 = 10;
-              }
-            this.atDest = false;
-          }
+			this.atDest = true;
+		} else {
+			if (this.atDest) {
+				// Piece just got freed
+				this.framesToUpd8 = 10;
+			}
+			this.atDest = false;
+		}
 		if (!this.atDest) {
 			this.pPos.x = this.pos.x;
 			this.pPos.y = this.pos.y;
@@ -70,20 +71,19 @@ class Piece {
 		}
 	}
 
-	newAngle(index, current, distance){
+	newAngle(index, current, d3) {
 		let newAngle = current;
-		const usableLength = this.xyAngles[index].length
+		const usableLength = this.xyAngles[index].length;
 		while (this.near(newAngle, current, 0.05) || this.near(newAngle, current + PI, 0.05)) {
 			//Keep choosing new angles until not at same or 180 flip
 			newAngle = this.xyAngles[index][floor(random(0, usableLength))];
 		}
 		this.vel.setHeading(newAngle);
-		if(index < 2){
-			this.framesToUpd8 = floor(distance / (cos(newAngle) * this.vel.mag()));
+		if (index < 2) {
+			this.framesToUpd8 = floor(d3 / (cos(newAngle) * this.vel.mag()));
 		} else {
-			this.framesToUpd8 = floor(distance / (sin(newAngle) * this.vel.mag()));
+			this.framesToUpd8 = floor(d3 / (sin(newAngle) * this.vel.mag()));
 		}
-		
 	}
 
 	setCourse(destX, destY) {
@@ -91,7 +91,6 @@ class Piece {
 		const lY = destY - this.pos.y;
 		const tXfY = abs(lX) > abs(lY) ? true : false;
 		const currAngle = this.vel.heading();
-
 		// X is the larger dimension
 		if (tXfY) {
 			const d3 = lX / this.pathDecay;
@@ -100,13 +99,17 @@ class Piece {
 			if (lX > 0) {
 				this.newAngle(0, currAngle, d3);
 			} else {
+				// Y dim is -
 				this.newAngle(1, currAngle, d3);
 			}
 		} else {
+			// Y is the larger
 			const d3 = lY / this.pathDecay;
+			// Y dim is +
 			if (lY > 0) {
 				this.newAngle(2, currAngle, d3);
 			} else {
+				// Y dim is -
 				this.newAngle(3, currAngle, d3);
 			}
 		}
